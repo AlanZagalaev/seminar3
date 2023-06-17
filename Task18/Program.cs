a8fb9f3d02e4fe3ccf7c7bbf891b0ad9c6442530
AlanZagalaev <manofword@mail.ru>
2023-06-17 21:04:37 +0300

    готовая домашка по семинару 3

---
+++ Task18/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введи координату x1");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи координату y1");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи координату z1");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи координату x2");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи координату y2");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи координату z2");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double skobX = Math.Pow(x1 - x2, 2);
+double skobY = Math.Pow(y1 - y2, 2);
+double skobZ = Math.Pow(z1 - z2, 2);
+double result = Math.Sqrt(skobX + skobY + skobZ);
+
+Console.WriteLine(result);
---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 19: Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом.
+
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.WriteLine("Введи пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99999 && number < 10000)
+{
+    Console.WriteLine("Вы ввели не пятизначное число");
+}
+    
+else if (number / 10000 == number % 10 && (number / 1000) % 10 == (number / 10) % 10)
+{
+    Console.WriteLine("вы ввели палиндром");
+}
+    else
+    {
+        Console.WriteLine("данное число не является палиндромом");
+    }
+Console.ReadLine();
